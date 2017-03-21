a1d0a86db48723ae1bf3323f1bf21f985c01cb6c
Joe Maples <joe@frap129.org>
2017-03-21 17:49:10 -0400

    Add Splash on open
    Signed-off-by: Joe Maples <joe@frap129.org>

---
+++ app/src/main/java/org/frap129/spectrum/SplashActivity.java
@@ -0,0 +1,25 @@
+package org.frap129.spectrum;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+public class SplashActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.splash);
+                final Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                                startActivity(intent);
+                                finish();
+                            }
+        }, 700);
+    }
+}
