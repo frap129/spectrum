a4f79e679d0b67dbd5b051948a9b92ba0ad17573
Joe Maples <joe@frap129.org>
2017-03-21 19:28:51 -0400

    Whitespace?

--- app/src/main/java/org/frap129/spectrum/SplashActivity.java
+++ app/src/main/java/org/frap129/spectrum/SplashActivity.java
@@ -5,21 +5,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-
 public class SplashActivity extends Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
-                setContentView(R.layout.splash);
-                final Handler handler = new Handler();
-                handler.postDelayed(new Runnable() {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-                                startActivity(intent);
-                                finish();
-                            }
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
         }, 700);
     }
 }
