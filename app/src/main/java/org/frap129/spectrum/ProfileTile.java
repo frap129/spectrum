3d8b6c27780d2becf30ae29e173fb0facba8e97f
Joe Maples <joe@frap129.org>
2017-04-23 19:59:08 -0400

    Not so minor cleanup
    Signed-off-by: Joe Maples <joe@frap129.org>

--- app/src/main/java/org/frap129/spectrum/ProfileTile.java
+++ app/src/main/java/org/frap129/spectrum/ProfileTile.java
@@ -16,67 +16,42 @@ public class ProfileTile extends TileService {
 
     @Override
     public void onStartListening() {
-        resetTileStatus();
+        updateTile();
     }
 
     @Override
     public void onClick() {
-        updateTile();
+        setProfile();
     }
 
-    private void updateTile() {
+    private void setProfile() {
         SharedPreferences profile = this.getSharedPreferences("profile", MODE_PRIVATE);
         SharedPreferences.Editor editor = profile.edit();
-        Tile tile = this.getQsTile();
         boolean isActive = getServiceStatus();
-        Icon newIcon;
-        String newLabel;
-        int newState;
 
         // Update tile and set profile
         if (isActive && click) {
-            newLabel = "Gaming";
-            newIcon = Icon.createWithResource(getApplicationContext(), R.drawable.game);
-            newState = Tile.STATE_ACTIVE;
-            click = false;
             MainActivity.setProfile(3);
             editor.putString("profile", "3");
             editor.apply();
         } else if (!isActive && click) {
-            newLabel = "Battery";
-            newIcon = Icon.createWithResource(getApplicationContext(), R.drawable.battery);
-            newState = Tile.STATE_ACTIVE;
-            click = true;
             MainActivity.setProfile(2);
             editor.putString("profile", "2");
             editor.apply();
         } else if (isActive && !click){
-            newLabel = "Performance";
-            newIcon = Icon.createWithResource(getApplicationContext(), R.drawable.rocket);
-            newState = Tile.STATE_ACTIVE;
-            click = true;
             MainActivity.setProfile(1);
             editor.putString("profile", "1");
             editor.apply();
         } else {
-            newLabel = "Balance";
-            newIcon = Icon.createWithResource(getApplicationContext(), R.drawable.atom);
-            newState = Tile.STATE_ACTIVE;
-            click = false;
             MainActivity.setProfile(0);
             editor.putString("profile", "0");
             editor.apply();
         }
 
-        // Change the UI of the tile.
-        tile.setLabel(newLabel);
-        tile.setIcon(newIcon);
-        tile.setState(newState);
-        tile.updateTile();
+        updateTile();
     }
 
     private boolean getServiceStatus() {
-
         SharedPreferences prefs = getApplicationContext().getSharedPreferences(PREFERENCES_KEY, MODE_PRIVATE);
         boolean isActive = prefs.getBoolean(SERVICE_STATUS_FLAG, false);
         isActive = !isActive;
@@ -86,7 +61,7 @@ public class ProfileTile extends TileService {
         return isActive;
     }
 
-    private void resetTileStatus() {
+    private void updateTile() {
         SharedPreferences profile = this.getSharedPreferences("profile", MODE_PRIVATE);
         Tile tile = this.getQsTile();
         Icon newIcon;
